14989ea3bfe114693d05f6ea94d8a38be50268da
tachyontechs <hafizabdullahqurashi1@gmail.com>
2023-04-02 14:55:59 +0500

    Devtron

---
+++ src/Components/Navbar.js
@@ -0,0 +1,60 @@
+import React, { useState } from "react";
+import './Scss/Navbar.scss'
+
+function Navbar() {
+  const [active, setActive] = useState("nav__menu");
+  const [icon, setIcon] = useState("nav__toggler");
+  const navToggle = () => {
+    if (active === "nav__menu") {
+      setActive("nav__menu nav__active");
+    } else setActive("nav__menu");
+
+    // Icon Toggler
+    if (icon === "nav__toggler") {
+      setIcon("nav__toggler toggle");
+    } else setIcon("nav__toggler");
+  };
+  return (
+   <div className="navbar-parent">
+     <nav className="nav">
+      <a href="#" className="nav__brand">
+       <h1>Devtron</h1>
+      </a>
+      <ul className={active}>
+        <li className="nav__item">
+          <a href="#" className="nav__link">
+            Home
+          </a>
+        </li>
+        <li className="nav__item">
+          <a href="#" className="nav__link">
+            About
+          </a>
+        </li>
+        <li className="nav__item">
+          <a href="#" className="nav__link">
+            Portfolio
+          </a>
+        </li>
+        <li className="nav__item">
+          <a href="#" className="nav__link">
+            Skills
+          </a>
+        </li>
+        <li className="nav__item">
+          <a href="#" className="nav__link">
+            Contact
+          </a>
+        </li>
+      </ul>
+      <div onClick={navToggle} className={icon}>
+        <div className="line1"></div>
+        <div className="line2"></div>
+        <div className="line3"></div>
+      </div>
+    </nav>
+   </div>
+  );
+}
+
+export default Navbar;
---
+++ src/Components/Reuseable/Slider.js
@@ -0,0 +1,61 @@
+import React, { useRef, useState } from "react";
+// Import Swiper React components
+import { Swiper, SwiperSlide } from "swiper/react";
+
+// Import Swiper styles
+import "swiper/css";
+import "swiper/css/effect-fade";
+import "swiper/css/navigation";
+import "swiper/css/pagination";
+
+import "./Slider.scss";
+
+// import required modules
+import { Autoplay, EffectFade, Navigation, Pagination } from "swiper";
+
+export default function Slider() {
+    return (
+        <>
+            <Swiper
+                spaceBetween={30}
+                effect={"fade"}
+                autoplay={{
+                    delay: 5000,
+                    disableOnInteraction: false,
+                }}
+                navigation={false}
+                pagination={false}
+                modules={[Autoplay, EffectFade, Navigation, Pagination]}
+                className="mySwiper"
+            >
+                <SwiperSlide>
+
+                    <img src={"./images/1.jpg"} width={"100%"} />
+
+
+                    <div className="parent">
+                        <div style={{width:'80%',margin:'auto'}}>
+                        <h1>Creating a Better <br /> <b style={{ color: '#2EDAF1' }}>IT Solutions.</b></h1>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas quis faucibus ligula. Maecenas a <br /> purus massa. Ut elementum sem mauris, ut venenatis tortor congue et. Ut et dolor massa.</p>
+
+                        <button><a href="">Contact Us </a></button>
+                        </div>
+                    </div>
+                </SwiperSlide>
+                <SwiperSlide>
+                    <img src={"./images/2.jpg"} width={"100%"} />
+                    <div className="parent">
+                        <div style={{width:'80%',margin:'auto'}}>
+                        <h1>Creating a Better <br /> <b style={{ color: '#2EDAF1' }}>IT Solutions.</b></h1>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas quis faucibus ligula. Maecenas a <br /> purus massa. Ut elementum sem mauris, ut venenatis tortor congue et. Ut et dolor massa.</p>
+
+                        <button><a href="">Contact Us </a></button>
+                        </div>
+                    </div>
+                </SwiperSlide>
+
+
+            </Swiper>
+        </>
+    );
+}
