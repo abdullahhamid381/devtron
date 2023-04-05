19a0cd130c0f2c9750e165847c69be16b4b52591
tachyontechs <hafizabdullahqurashi1@gmail.com>
2023-04-06 01:05:23 +0500

    update

--- src/Components/TrustedCompany.js
+++ src/Components/TrustedCompany.js
@@ -8,12 +8,37 @@ const TrustedCompany = () => {
                     <h3>
                         Trusted By <br /> <b>Big Companies</b></h3>
                 </div>
-                
-                    <img src="./images/10.png" alt="" />
-                    <img src="./images/11.png" alt="" />
-                    <img src="./images/13.png" alt="" />
-                    <img src="./images/14.png" alt="" />
-                
+
+                <img src="./images/10.png" alt="" />
+                <img src="./images/11.png" alt="" />
+                <img src="./images/13.png" alt="" />
+                <img src="./images/14.png" alt="" />
+
+            </div>
+
+            <div className='detail'>
+                <div className='img-div'>
+                    <img src={'./images/16.jpg'} alt="" />
+                </div>
+                <div className='title'>
+                    <h1>
+                        Transform Your Business <br />
+                        <b>With AI Technology</b>
+                    </h1>
+
+                    <div className='para'>
+                        <div className='para-button'>
+                            <p>
+                                Lorem veniam, quis nostrud exercitation ullamco laboris. Sed uteniri perspiciatis unde omnis iste natus error sit voluptatem accusant doloremque laudantium
+                                <br />
+                                <button>Learn More</button>
+                            </p>
+                        </div>
+                        <div className='img-sub'>
+                            <img src={'./images/17.jpg'} alt="" />
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
     )
