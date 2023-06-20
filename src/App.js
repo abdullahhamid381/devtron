1df2346df4c63445b607121352ca99ba8ec71e0e
tachyontechs <hafizabdullahqurashi1@gmail.com>
2023-06-20 21:12:36 +0500

    update

--- src/App.js
+++ src/App.js
@@ -19,8 +19,8 @@ const App = () => {
        <Counterup start={0} end={500} starttwo={0} endtwo={15} />
      <TrustedCompany />
      <BestService />
-       {/* <CompletedProject />
-      <Reviews />
+       <CompletedProject />
+       {/*<Reviews />
       <History start={0} end={200} starttwo={0} endtwo={100} startthree={0} endthree={10} startfour={0} endfour={24} />
       <Blog/>
       <Footer/> */}
